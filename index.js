46c629e1aa8c465f71937b685a9fa0514100c707
Mike Tsao <mike@sowbug.com>
2015-02-05 12:04:45 +0000

    Add SW.

---
+++ index.js
@@ -0,0 +1,7 @@
+navigator.serviceWorker.register('/sw.js')
+  .then(function(registration) {
+    console.log('ServiceWorker registration successful with scope: ',
+                registration.scope);
+  }).catch(function(err) {
+    console.log('ServiceWorker registration failed: ', err);
+  });
