8d554b65ccbf77aad9ca6a91692d1b9b9274a20e
Mike Tsao <mike@sowbug.com>
2015-02-05 14:24:24 +0000

    Trailing slash on scope

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-navigator.serviceWorker.register('/fizz-hackathon-2015/sw.js',
-                                 { scope: '/fizz-hackathon-2015' }
-                                )
-  .then(function(registration) {
-    console.log('ServiceWorker registration successful with scope: ',
-                registration.scope);
-  }).catch(function(err) {
-    console.log('ServiceWorker registration failed: ', err);
-  });
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/fizz-hackathon-2015/sw.js',
+                                   { scope: '/fizz-hackathon-2015/' }
+                                  )
+    .then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ',
+                  registration.scope);
+    }).catch(function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
